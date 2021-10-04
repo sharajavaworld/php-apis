a6454a075ea4f9e490642b8799deb3ad8431186c
sharajavaworld <sharajavaworld@gmail.com>
2021-10-04 14:33:25 +0530

    Adding new code

--- index.php
+++ index.php
@@ -2,19 +2,19 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require_once 'curl.php';
+require_once 'redis_adapter.php';
 echo "<h1>Welcome to PHP World.</h1>";
 
-$redisObject = new curl();
+$redisAdapter = new redisAdapter();
 
 
-$redisObject->setValue(
+$redisAdapter->setValue(
     'sharak', 
     $url = 'http://investocart.mindstack.in/wordpress/wp-json/wp/v2/home_page_intro/4821',
     $method = "GET"
 );
 
-$value = $redisObject->getValue('sharak');
+$value = $redisAdapter->getValue('sharak');
 echo '===> '. $value;
 
 ?>
