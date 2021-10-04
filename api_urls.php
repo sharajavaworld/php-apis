60a3f8f8aaf89176198e57a52467486134f11bb4
sharajavaworld <sharajavaworld@gmail.com>
2021-10-04 14:39:09 +0530

    Adding new code

--- api_urls.php
+++
@@ -1,4 +0,0 @@
-<?php
-const HOME_PAGE_INTRO = 'http://investocart.mindstack.in/wordpress/wp-json/wp/v2/home_page_intro/4821';
-
-?>
--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+
 require_once 'redisAdapter.php';
+require_once 'apiUrls.php';
+
 
 echo "<br>";
 echo "<h1>Welcome to PHP World.</h1>";
@@ -15,11 +19,11 @@ $redisAdapter = new redisAdapter();
 
 $redisAdapter->setValue(
     'sharak', 
-    $url = 'http://investocart.mindstack.in/wordpress/wp-json/wp/v2/home_page_intro/4821',
+    $url = HOME_PAGE_INTRO,
     $method = "GET"
 );
 
 $value = $redisAdapter->getValue('sharak');
-echo '===> '. $value;
+echo '===> <br>'. $value;
 
 ?>
