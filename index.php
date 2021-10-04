bef49531ec00d87263ef060689e720fe0a53bf30
sharajavaworld <sharajavaworld@gmail.com>
2021-10-04 14:34:28 +0530

    Adding new code

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require_once 'redis_adapter.php';
+require_once 'redisAdapter.php';
 echo "<h1>Welcome to PHP World.</h1>";
 
 $redisAdapter = new redisAdapter();
